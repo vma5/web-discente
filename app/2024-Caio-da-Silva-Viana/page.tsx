97aef7e9e4ba9eec0a058f0e7b74e0c5691787cb
Caio da Silva <caiosil.viana@gmail.com>
2024-11-11 13:50:32 -0300

    Corrigindo o erro

--- app/2024-Caio-da-Silva-Viana/page.tsx
+++ app/2024-Caio-da-Silva-Viana/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div>
             Link para o meu GitHub &#128073; 
-            <a href="https://github.com/CaioSilvaCsv" class = "hover:bg-blue-200 rounded-lg decoration-transparent no-underline p-0.5 hover:text-gray-800 ">
+            <a href="https://github.com/CaioSilvaCsv" className="hover:bg-blue-200 rounded-lg p-0.5 hover:text-gray-800 ">
                 Caio da Silva Viana
             </a>
       </div >
