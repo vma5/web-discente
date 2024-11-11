f2f7cf10f27371a5b93ac3c0b022f78c7f9a14b4
Caio da Silva <caiosil.viana@gmail.com>
2024-11-11 00:42:29 -0300

    add name Caio in app\2024-Caio-da-Silva-Viana\page.tsx

---
+++ app/2024-Caio-da-Silva-Viana/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div>
+            Link para o meu GitHub &#128073; 
+            <a href="https://github.com/CaioSilvaCsv" class = "hover:bg-blue-200 rounded-lg decoration-transparent no-underline p-0.5 hover:text-gray-800 ">
+                Caio da Silva Viana
+            </a>
+      </div >
+    );
+  }
