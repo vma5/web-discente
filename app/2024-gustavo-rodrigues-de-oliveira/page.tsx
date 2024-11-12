25d2e1527923746da945997a85cdd051035c368f
Gustavo <104570961+gurodrigues96@users.noreply.github.com>
2024-11-12 11:07:47 -0300

    Add files via upload

---
+++ app/2024-gustavo-rodrigues-de-oliveira/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div>
+        Link para o meu GitHub &#128073; 
+        <a href="https://github.com/gurodrigues96">
+            Gustavo Rodrigues de Oliveira
+        </a>
+      </div >
+    );
+  }
